aa0306be87b7f55a622b012cc81dd552a0213a54
Ingrid <ingridgti08@hotmail.com>
2025-07-25 19:55:48 -0300

    chore: adjust eslint

--- eslint.config.js
+++ eslint.config.js
@@ -11,7 +11,7 @@ import { globalIgnores } from "eslint/config";
 export default tseslint.config([
   globalIgnores(["dist"]),
   {
-    files: ["src/*/.{ts,tsx}"],
+    files: ["src/*/.{ts,tsx}", ".storybook/*/.{ts,tsx}"],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
